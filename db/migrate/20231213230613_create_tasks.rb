9ff63c993835a5987fbe524035243ec62abec3ee
Julian Pormentilla <julian.pormentilla@gmail.com>
2023-12-13 18:23:14 -0500

    Generated Tasks Controller.

--- db/migrate/20231213230613_create_tasks.rb
+++ db/migrate/20231213230613_create_tasks.rb
@@ -1,7 +1,8 @@
 class CreateTasks < ActiveRecord::Migration[7.1]
   def change
     create_table :tasks do |t|
-      
+      t.string :description
+      t.boolean :completed, default: false 
 
       t.timestamps
     end
