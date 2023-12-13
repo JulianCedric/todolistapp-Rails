7a2075e8782780b47130229fd92c4c9a23aec8d0
Julian Pormentilla <julian.pormentilla@gmail.com>
2023-12-13 18:57:41 -0500

    Implemented CRUD Actions

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,35 @@
 class TasksController < ApplicationController
-end
+    def index
+        @tasks = Task.all
+        render json: @tasks 
+    end
+  
+    def create
+        @task = Task.new(task_params)
+        if @task.save
+            render json: @task, status: :created, location: @task
+        else
+            render json: @task.errors, status: :unprocessable_entity
+        end
+    end      
+  
+    def update
+        @task = Task.find(params[:id])
+        if @task.update(task_params)
+            render json: @task
+        else
+            render json: @task.errors, status: :unprocessable_entity
+        end
+    end      
+  
+    def destroy
+        @task = Task.find(params[:id])
+        @task.destroy
+        head :no_content
+    end
+      
+    private
+    def task_params
+        params.require(:task).permit(:description, :completed)
+    end
+  end
